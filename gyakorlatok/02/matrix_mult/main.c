cf959e7f5159ac34fb9de2234591d441ce777344
Lengyel Gábor <lengyelgabor23@gmail.com>
2025-03-17 17:10:12 +0100

    Generate the matrices with random numbers

--- gyakorlatok/02/matrix_mult/main.c
+++ gyakorlatok/02/matrix_mult/main.c
@@ -4,58 +4,61 @@
 
 #include <CL/cl.h>
 
-const char* kernel_code =
-    "__kernel void matrix_mult(__global int* A, __global int* B, __global int* C, int n) {\n"
-    "   int row = get_global_id(0);\n"
-    "   int col = get_global_id(1);\n"
-    "   if (row < n && col < n) {\n"
-    "       int sum = 0;\n"
-    "       for (int k = 0; k < n; k++) {\n"
-    "           sum += A[row * n + k] * B[k * n + col];\n"
-    "       }\n"
-    "       C[row * n + col] = sum;\n"
-    "   }\n"
-    "}\n";
-
 #define MATRIX_SIZE 3
 
 char* loadKernelFromFile(const char* filename, size_t* kernel_size);
 void printMatrix(int* matrix, int size);
 int randInt(int min, int max);
+void generateMatrix(int* matrix, int size, int min, int max);
 
 
 int main(void) {
     cl_int err;
     const int matrix_size = MATRIX_SIZE;
 
-    int A[MATRIX_SIZE*MATRIX_SIZE] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-    int B[MATRIX_SIZE*MATRIX_SIZE] = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+    int A[MATRIX_SIZE*MATRIX_SIZE];
+    int B[MATRIX_SIZE*MATRIX_SIZE];
     int C[MATRIX_SIZE*MATRIX_SIZE];
 
     srand(time(NULL));
+    generateMatrix(A, MATRIX_SIZE, 1, 10);
+    generateMatrix(B, MATRIX_SIZE, 1, 10);
 
-    int rand = randInt(1, 10);
-    printf("Random: %d\n", rand);
-    rand = randInt(1, 10);
-
-    // OpenCL inicializálás
+    // Get platform
+    cl_uint n_platforms;
     cl_platform_id platform_id;
-    cl_device_id device_id;
-    cl_uint n_platforms, n_devices;
     err = clGetPlatformIDs(1, &platform_id, &n_platforms);
-    err |= clGetDeviceIDs(platform_id, CL_DEVICE_TYPE_GPU, 1, &device_id, &n_devices);
+    if (err != CL_SUCCESS) {
+        printf("[ERROR] Error calling clGetPlatformIDs. Error code: %d\n", err);
+        return 0;
+    }
 
-    // OpenCL kontextus létrehozása
+    // Get device
+    cl_device_id device_id;
+    cl_uint n_devices;
+    err = clGetDeviceIDs(
+        platform_id,
+        CL_DEVICE_TYPE_GPU,
+        1,
+        &device_id,
+        &n_devices
+    );
+    if (err != CL_SUCCESS) {
+        printf("[ERROR] Error calling clGetDeviceIDs. Error code: %d\n", err);
+        return 0;
+    }
+
+    // Create OpenCL context
     cl_context context = clCreateContext(NULL, 1, &device_id, NULL, NULL, &err);
 
-    // Kernel fájlból betöltése
+    // Load kernel from file
     size_t kernel_size;
     char* kernel_source = loadKernelFromFile("matrix_mult.cl", &kernel_size);
     if (!kernel_source) {
         return -1;
     }
 
-    // Program és kernel létrehozása
+    // Build the program
     cl_program program = clCreateProgramWithSource(context, 1, (const char**)&kernel_source, &kernel_size, &err);
     free(kernel_source);
 
@@ -139,3 +142,9 @@ int randInt(int min, int max) {
     return min + rand() % (max - min + 1);
 }
 
+
+void generateMatrix(int* matrix, int size, int min, int max) {
+    for (int i = 0; i < size * size; i++) {
+        matrix[i] = randInt(min, max);
+    }
+}
